c3ec933dba97cd963c13b18da0ad61a9e7b276eb
Nik Cherednik <ighosta9@gmail.com>
2022-09-07 06:57:30 +0000

    [Prototype] Playing around with pointers.
    - Just used env as a sandbox - no useful changes.
    - Saved.

--- LinkedList/src/main.cpp
+++ LinkedList/src/main.cpp
@@ -1,33 +1,98 @@
 #include <iostream>
 
-template<class T> 
-class Node {
-  private:
-    Node<T> *next_;
-    T data_;
-  public:
-    Node() : next_(nullptr), data_() {
-      std::cout << "its address: " << this << " " << data_ << "\n";
-    };
-
-    Node(const T& value) : next_(nullptr), data_(value) {
-    };
-};
-
 template<class T>
 class LinkedList {
   private:
-    Node<T> head_;
+
+    struct Head;
+    struct Node;
+
+    struct Head {
+      protected:
+        // Node* next_;
+
+      public:
+        Node* next_;
+
+        Head() : next_(nullptr) {}
+
+        Head(Node &next) : next_(&next) {}
+  
+        void set_next(Node *node) {
+          next_ = node;
+        }
+  
+        Node* next() {
+          return next_;
+        }
+    };
+
+    struct Node : public Head {
+      private:
+        T data_;
+
+      public:
+        Node() : Head(), data_() {
+          std::cout << "its address: " << this << " " << data_ << "\n";
+        };
+    
+        Node(const T &value) : Head(), data_(value) {
+          std::cout << "its address: " << this << " " << data_ << "\n";
+        };
+  
+        Node(const T &value, const Node &next) : Head(&next), data_(value) {
+          std::cout << "its address: " << this << " " << data_ << "\n";
+        }
+
+        T data() {
+          return data_;
+        }
+    };
+
+    Head head_;
     std::size_t size_;
 
   public:
-    LinkedList<T>() : size_(0) {}
+    LinkedList<T>() : size_(0), head_() {}
 
-    LinkedList<T>(const std::initializer_list<T>& l) : size_(0), head_() {
+    LinkedList<T>(const std::initializer_list<T>& list) : size_(0), head_() {
+      // Node *currentNode = head_.next();
+      Node **currentNode = &(head_.next_);
+
+      for (const T& item : list) {
+        *currentNode = new Node(item);
+        // (*currentNode).set_next(currentNode);
+        // currentNode = (*currentNode).next();
+        currentNode = *&currentNode;
+
+        ++size_;
+      }
+
+      auto node = head_.next_;
+      while (node != NULL) {
+        std::cout << node->data() << "\n";
+        node = node->next_;
+      }
+
+      // temp = "132";
+      // std::cout << (*a).data() << "\n";
+
+/*
       std::cout << "header address: " << &head_ << "\n";
+      std::cout << "pointer to by address: " << &a << "\n";
+      std::cout << "node address: " << &*a << "\n";
+      head_.set_next(a);
+*/
     }
 
-    void push_front(T value);
+    void push_front(T value) {
+      /*
+      Node temp = new Node(value);
+      head_.set_next(&temp);
+      ++size_;
+      */
+    }
+
     void push_back(T value);
     void pop_front(T value);
     void pop_back(T value);
@@ -36,7 +101,11 @@ class LinkedList {
     void remove_at(size_t index);
     LinkedList<T> reverse(size_t index);
     std::size_t find(T value) const;
-    T front() const;
+
+    T front() const {
+      // return (*(head_.next())).data();
+    };
+
     T back() const;
     bool is_empty() const;
     std::size_t size() const;
@@ -44,4 +113,7 @@ class LinkedList {
 
 int main() {
   LinkedList<std::string> a{"a", "b"};
+  // a.push_front("hey!");
+  // std::cout << (*&a.front());
+  // std::cout << a.front();
 }
