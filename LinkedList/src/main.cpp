29e094bd3abfbf6c11b4cae9ac301ada59820e7b
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2022-09-11 15:05:18 +0300

    [Add] Iterators support; gitignore file cleaned

--- LinkedList/src/main.cpp
+++ LinkedList/src/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+#include <chrono>
 #include <iterator>
 #include <cassert>
+#include <forward_list>
 
 template<class T>
 class LinkedList {
     struct Node {
     public:
-        T data_{};
+        T data{};
         Node* next = nullptr;
 
         Node() = default;
 
-        Node(T value, Node* next) : data_(value), next(next) {}
+        Node(T value, Node* next) : data(value), next(next) {}
     };
 
     Node head_{};
     // Tail tail_;
     std::size_t size_ = 0;
+
 
     void cleanup() {
         /*
@@ -40,6 +43,68 @@ class LinkedList {
     }
 
 public:
+    template<class ValueType>
+    class ForwardIterator {
+    private:
+        Node* node_ = nullptr;
+        friend class LinkedList;
+
+        explicit ForwardIterator(Node* node) : node_(node) {};
+    public:
+        // using iterator_category = std::forward_iterator_tag;
+        // using value_type = ValueType;
+        // using difference_type = std::ptrdiff_t;
+        using pointer = ValueType*;
+        using reference = ValueType&;
+
+        ForwardIterator() = default;
+
+        ForwardIterator(const ForwardIterator<T>& source) noexcept : node_(source.node_) {}
+
+        ForwardIterator& operator=(const ForwardIterator& rhs) = default;
+
+        bool operator==(const ForwardIterator<const T>& rhs) {
+            return this->node_ == rhs.node_;
+        };
+
+        bool operator==(const ForwardIterator<T>& rhs) {
+            return this->node_ == rhs.node_;
+        };
+
+        bool operator!=(const ForwardIterator<const T>& rhs) {
+            return !(*this == rhs);
+        };
+
+        bool operator!=(const ForwardIterator<T>& rhs) {
+            return !(*this == rhs);
+        };
+
+        ForwardIterator& operator++() {
+            assert(node_ != nullptr);
+            node_ = node_->next;
+            return *this;
+        }
+
+        ForwardIterator operator++(int) noexcept {
+            ForwardIterator old_value(*this);
+            ++(*this);
+            return old_value;
+        }
+
+        reference operator*() const noexcept {
+            assert(node_ != nullptr);
+            return node_->data;
+        }
+
+        pointer operator->() const noexcept {
+            assert(node_ != nullptr);
+            return node_->data;
+        }
+    };
+
+    using Iterator = ForwardIterator<T>;
+    using ConstIterator = ForwardIterator<const T>;
+
     LinkedList() = default;
 
     /*
@@ -64,7 +129,23 @@ public:
         cleanup();
     }
 
-    void insert(T data, int index) {
+    Iterator begin() const noexcept {
+        return Iterator{head_.next};
+    }
+
+    Iterator end() const noexcept {
+        return Iterator{};
+    }
+
+    ConstIterator cbegin() const noexcept {
+        return ConstIterator{head_.next};
+    }
+
+    ConstIterator cend() const noexcept {
+        return ConstIterator{};
+    }
+
+    void insert(const T& data, int index) {
         Node** elem_before = find_at(index - 1);
 
         *elem_before = new Node(data, *elem_before);
@@ -72,7 +153,7 @@ public:
         ++size_;
     }
 
-    void push_front(T value) {
+    void push_front(const T& value) {
         insert(value, 0);
     }
 
@@ -86,21 +167,20 @@ public:
 
     void remove(T value) {}
 
-    void remove_at(size_t index) {}
+    void remove_at(int index) {}
 
-    LinkedList<T> reverse(size_t index) {}
+    LinkedList reverse(size_t index) {}
 
     // Returns index if found, else -1
     int find(const T& value) const noexcept {
 
     }
 
-    // TODO: as iterators and getters
-    T front() const noexcept {
-
+    Node& front() const noexcept {
+        return *(head_.next);
     }
 
-    T back() const noexcept {
+    T& back() const noexcept {
 
     }
 
@@ -113,14 +193,24 @@ public:
     }
 };
 
-int* test() {
-    int* a = new int();
-    *a = 200;
-    return a;
-}
+struct test {
+    int some_data = 10;
+};
 
 int main() {
     LinkedList<std::string> a{};
-    a.push_front("Hello!");
-    a.push_back("Bye!");
+    a.push_front("Hey");
+    a.push_front("Hey2");
+    a.push_front("Hey5");
+    a.push_back("bye...");
+
+    for (const std::string& item : a) {
+        std::cout << item << std::endl;
+    }
+    /*
+    while (it != a.end()) {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+     */
 }
